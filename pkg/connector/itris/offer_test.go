94b2c1c4c10b3d2b12d2b4e046dc92e4ef8d1b3e
Julien Robert <julien@rbrt.fr>
2020-12-20 23:00:56 +0100

    Implementing itris connector: add housing type parsing

--- pkg/connector/itris/offer_test.go
+++ pkg/connector/itris/offer_test.go
@@ -3,21 +3,21 @@ package itris_test
 import (
 	"testing"
 
-	"github.com/woningfinder/woningfinder/internal/corporation"
+	"github.com/woningfinder/woningfinder/pkg/connector/itris"
 
-	"github.com/woningfinder/woningfinder/internal/bootstrap"
+	"github.com/woningfinder/woningfinder/internal/corporation"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_FetchOffer(t *testing.T) {
 	a := assert.New(t)
-	itrisConnector := bootstrap.CreateOnsHuisClient()
+	itrisConnector := itris.NewConnector("https://mijn.woongoedzvl.nl")
 
 	offers, err := itrisConnector.FetchOffer()
 	a.NoError(err)
 	a.True(len(offers) > 0)
-	for _, offer := range offers {
+	for i, offer := range offers {
 		a.NotEmpty(offer.Housing.Type.Type)
 		if offer.Housing.Type.Type == corporation.Undefined {
 			continue
@@ -33,14 +33,14 @@ func Test_FetchOffer(t *testing.T) {
 		a.NotEmpty(offer.Housing.Address)
 		a.NotEmpty(offer.Housing.EnergieLabel)
 		a.True(offer.Housing.BuildingYear > 0)
-		if offer.Housing.Size == 0 {
-			continue
-		}
-		a.True(offer.Housing.Size > 0)
 		a.True(offer.Housing.Price > 0)
 		a.True(offer.Housing.Longitude > 0)
 		a.True(offer.Housing.Latitude > 0)
 		a.True(offer.Housing.NumberRoom > 0)
+		if offer.Housing.Size == 0 && len(offers) > i-1 {
+			continue
+		}
+		a.True(offer.Housing.Size > 0)
 
 		// test only for one offer
 		return
