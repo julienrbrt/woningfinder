83369d554ecf66e860b4976313d45023be0666cf
Julien Robert <julien@rbrt.fr>
2021-04-26 00:41:44 +0200

    Improve checks between Basis vs Pro plan (Closes #36)

--- internal/customer/matcher/passend_toewijzen.go
+++ internal/customer/matcher/passend_toewijzen.go
@@ -7,7 +7,7 @@ import (
 	"github.com/woningfinder/woningfinder/internal/customer"
 )
 
-// last updated in 2021
+// last updated in april 2021
 
 // https://www.woningmarktbeleid.nl/onderwerpen/huurtoeslag
 // https://www.woningmarktbeleid.nl/actueel/nieuws/2020/11/18/inkomens--en-huurgrenzen-huurtoeslag-2021-bekend
@@ -15,10 +15,10 @@ const (
 	aow   = 67
 	young = 23
 
-	kwaliteitskortingsgrens = 442.46
-	aftoppingsgrenslaag     = 633.25
-	aftoppingsgrenshoog     = 678.66
-	maximalehuurgrens       = 752.33
+	Kwaliteitskortingsgrens = 442.46
+	Aftoppingsgrenslaag     = 633.25
+	Aftoppingsgrenshoog     = 678.66
+	Maximalehuurgrens       = 752.33
 )
 
 // PassendToewijzen determines the rent range to which an user can react
@@ -30,42 +30,42 @@ func (m *matcher) passendToewijzen(user customer.User) (float64, float64) {
 
 	// bypass passend toewijzen
 	if user.YearlyIncome == -1 {
-		return 0, math.MaxInt16
+		return 0, math.MaxInt32
 	}
 
 	// if too rich free sector only allowed
-	if user.YearlyIncome > 44655 {
-		return maximalehuurgrens, math.MaxInt16
+	if user.YearlyIncome > customer.MaximumIncomeSocialHouse {
+		return Maximalehuurgrens, math.MaxInt32
 	}
 
 	// housing allowance for people younger than 23 years old and no children
 	if age < young && !user.HasChildrenSameHousing {
-		return 0, kwaliteitskortingsgrens
+		return 0, Kwaliteitskortingsgrens
 	}
 
 	switch {
 	case user.FamilySize <= 1:
 		if user.YearlyIncome <= 23725 {
-			return 0, aftoppingsgrenslaag
+			return 0, Aftoppingsgrenslaag
 		} else if user.YearlyIncome <= 32200 {
-			return 0, aftoppingsgrenshoog
+			return 0, Aftoppingsgrenshoog
 		} else if user.YearlyIncome <= 40024 {
-			return aftoppingsgrenshoog, maximalehuurgrens
+			return Aftoppingsgrenshoog, Maximalehuurgrens
 		}
 	case user.FamilySize == 2:
 		if user.YearlyIncome <= 32200 {
-			return 0, aftoppingsgrenslaag
+			return 0, Aftoppingsgrenslaag
 		} else if user.YearlyIncome <= 40024 {
-			return aftoppingsgrenshoog, maximalehuurgrens
+			return Aftoppingsgrenshoog, Maximalehuurgrens
 		}
 	case user.FamilySize >= 3:
 		if user.YearlyIncome <= 32200 {
-			return 0, aftoppingsgrenshoog
+			return 0, Aftoppingsgrenshoog
 		} else if user.YearlyIncome <= 40024 {
-			return aftoppingsgrenshoog, maximalehuurgrens
+			return Aftoppingsgrenshoog, Maximalehuurgrens
 		}
 	}
 
 	// minimum allowed to rent via woningcorporation
-	return aftoppingsgrenshoog, math.MaxInt16
+	return Aftoppingsgrenshoog, math.MaxInt32
 }
