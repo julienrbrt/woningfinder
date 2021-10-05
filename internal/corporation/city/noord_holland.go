01384c4a10167d6b258692d911264d378a4d01a9
Julien Robert <julien@rbrt.fr>
2021-10-05 12:00:48 +0200

    Implement WoningNet Amsterdam (#100) (#133)

--- internal/corporation/city/noord_holland.go
+++ internal/corporation/city/noord_holland.go
@@ -1,6 +1,96 @@
 package city
 
+var Aalsmeer = City{
+	Name:        "Aalsmeer",
+	Coordinates: []float64{4.761114, 52.265986},
+}
+
+var Amsterdam = City{
+	Name:        "Amsterdam",
+	Coordinates: []float64{4.900112, 52.371009},
+}
+
+var Diemen = City{
+	Name:        "Diemen",
+	Coordinates: []float64{4.960929, 52.342095},
+}
+
+var Hoofddorp = City{
+	Name:        "Hoofddorp",
+	Coordinates: []float64{4.667389, 52.302815},
+}
+
+var Krommenie = City{
+	Name:        "Krommenie",
+	Coordinates: []float64{4.76612, 52.49528},
+}
+
+var Kudelstaart = City{
+	Name:        "Kudelstaart",
+	Coordinates: []float64{4.750673, 52.233007},
+}
+
+var Landsmeer = City{
+	Name:        "Landsmeer",
+	Coordinates: []float64{4.916186, 52.430915},
+}
+
+var Marken = City{
+	Name:        "Marken",
+	Coordinates: []float64{5.106826, 52.457875},
+}
+
+var NieuwVennep = City{
+	Name:        "Nieuw-Vennep",
+	Coordinates: []float64{4.621429, 52.26898},
+}
+
+var Oostzaan = City{
+	Name:        "Oostzaan",
+	Coordinates: []float64{4.873038, 52.438192},
+}
+
+var OuderkerkAanDeAmstel = City{
+	Name:        "Ouderkerk aan de Amstel",
+	Coordinates: []float64{4.916469, 52.294668},
+}
+
+var Purmerend = City{
+	Name:        "Purmerend",
+	Coordinates: []float64{4.961483, 52.502479},
+}
+
+var Uithoorn = City{
+	Name:        "Uithoorn",
+	Coordinates: []float64{4.82511, 52.241141},
+}
+
+var Vijfhuizen = City{
+	Name:        "Vijfhuizen",
+	Coordinates: []float64{4.67818, 52.35085},
+}
+
+var Wormer = City{
+	Name:        "Wormer",
+	Coordinates: []float64{4.859343, 52.505552},
+}
+
+var Zwanenburg = City{
+	Name:        "Zwanenburg",
+	Coordinates: []float64{4.743744, 52.377864},
+}
+
+var Amstelveen = City{
+	Name:        "Amstelveen",
+	Coordinates: []float64{4.856535, 52.303398},
+}
+
 var Bussum = City{
 	Name:        "Bussum",
 	Coordinates: []float64{5.162747, 52.277421},
+}
+
+var Zaandam = City{
+	Name:        "Zaandam",
+	Coordinates: []float64{4.829861, 52.442493},
 }
