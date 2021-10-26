3f1b4a6b41b661dfde21ef33f6eca606ff29f425
Julien Robert <julien@rbrt.fr>
2021-10-26 16:23:31 +0200

    Use transactional email service (Closes #190) (#195)

--- internal/bootstrap/email_client.go
+++ internal/bootstrap/email_client.go
@@ -7,10 +7,5 @@ import (
 
 // CreateEmailClient creates an email client
 func CreateEmailClient() email.Client {
-	return email.NewClient(config.MustGetString("APP_NAME"),
-		config.MustGetString("EMAIL_ADDRESS"),
-		config.MustGetString("EMAIL_PASSWORD"),
-		config.MustGetString("EMAIL_SMTP_ADDRESS"),
-		config.MustGetInt("EMAIL_SMTP_PORT"),
-	)
+	return email.NewClient(config.MustGetString("POSTMARK_SERVER_KEY"))
 }
