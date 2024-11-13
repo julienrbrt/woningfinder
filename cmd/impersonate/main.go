ae4455e46cd2d0c95726100e5ef019abbca2c047
Julien Robert <julien@rbrt.fr>
2024-11-13 10:25:28 +0100

    delete unecessary env variable

--- cmd/impersonate/main.go
+++ cmd/impersonate/main.go
@@ -14,13 +14,12 @@ import (
 	"go.uber.org/zap"
 )
 
-// init is invoked before main()
 func init() {
 	// loads values from .env into the system
 	// fallback to system env if unexisting
 	// if not defined on system, panics
 	if err := godotenv.Load(); err != nil {
-		_ = config.MustGetString("APP_NAME")
+		_ = config.MustGetString("DATABASE_URL")
 	}
 }
 
